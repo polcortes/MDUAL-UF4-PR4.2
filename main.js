d1e11f5f16dcd8f632480c07e1a889404764dc65
polcortes <pcortesgarcia.cf@iesesteveterradas.cat>
2023-10-09 20:20:35 +0200

    primeros archivos

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-const 
+const express = require('express');
+const ejs = require("ejs");
+const url = require("url");
+const app = express();
+const port = 3000;
+const fs = require('fs/promises');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 } // Tamaño máximo en bytes (1 MB en este ejemplo)
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// cosas
+
+// Activar el servidor
+const httpServer = app.listen(port, appListen)
+function appListen () {
+    console.log(`Example app listening on: http://localhost:${port}`)
+}
